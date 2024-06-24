33e8af35c9abab49c4f93961eb7dafdea2f41c3e
perwezroshan <157289267+perwezroshan@users.noreply.github.com>
2024-06-24 19:54:13 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import { AboutPage } from './Pages/AboutPage'
 import { AcadamicPage } from './Pages/AcadamicPage'
 import { ContactUsPage } from './Pages/ContactUsPage'
 import { Signin } from './Pages/Signin'
+import {Class9}   from './Pages/Class9Page'
+import {Class10}   from './Pages/Class10Page'
+import {Class11}   from './Pages/Class11Page'
+import {Class12}   from './Pages/Class12Page'
 import { Signup } from './Pages/Signup'
 import { Footer } from './component/Footer'
 
@@ -24,6 +28,11 @@ function App() {
         <Route path="/contact" element={<ContactUsPage/>} />
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/signin" element={<Signin/>} />
+        <Route path="/class9" element={<Class9 />} />
+        <Route path="/class10" element={<Class10 />} />
+        <Route path="/class11" element={<Class11 />} />
+        <Route path="/class12" element={<Class12 />} />
+
         <Route path="*" element={<div>Not found Page</div>} />
       </Routes>
       <Footer/>

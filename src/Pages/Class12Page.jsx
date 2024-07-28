327eed169171382b1f8f96fed9dc60942d447165
perwezroshan <157289267+perwezroshan@users.noreply.github.com>
2024-07-28 11:52:49 +0530

    class 12th responsiveness

--- src/Pages/Class12Page.jsx
+++ src/Pages/Class12Page.jsx
@@ -8,28 +8,27 @@ export const Class12 = () => {
   const Rectangle50 = "https://res.cloudinary.com/dh26dmbg3/image/upload/v1719234801/Rectangle_50_nidzhb.png";
 
   return (
-    <div className='flex flex-col mx-auto mt-5'>
+    <div className='flex flex-col mx-auto mt-5 items-center justify-center sm:max-w-screen-md sm:px-0 px-4'>
       
-      <div className='w-full flex items-center mb-5'>
-        <h1 className='text-5xl font-bold mr-auto' 
-          >Class 12</h1> 
+      <div className='flex items-center mb-5'>
+        <h1 className='sm:text-5xl text-3xl font-bold text-center'>Class 12</h1> 
       </div>
       
-      <div className='flex flex-wrap'>
-        <div className='w-1/2 p-[106px]'>
-          <div className='w-[407px] h-[301px] text-5xl font-itim mb-[107px] cursor-pointer' onClick={()=>{window.open("https://drive.google.com/drive/folders/13wp8UX9wrkPs3-FgmUPGv6ANaSSu3gap?usp=sharing",'_blank')}}>
-            <img src={Rectangle65} alt="maths" />
+      <div className='flex flex-wrap justify-center sm:justify-between'>
+        <div className='flex flex-col items-center p-10 sm:w-1/2 w-full'>
+          <div className='flex flex-col items-center mb-8 cursor-pointer' onClick={() => { window.open("https://drive.google.com/drive/folders/13wp8UX9wrkPs3-FgmUPGv6ANaSSu3gap?usp=sharing", '_blank') }}>
+            <img src={Rectangle65} alt="maths" className='w-full mb-16 h-auto sm:max-w-none max-w-xs' />
           </div>
-          <div className='w-[407px] h-[301px] text-5xl font-itim cursor-pointer' onClick={()=>{window.open("https://drive.google.com/drive/folders/1abOl4ymu54b-s4M28tVHv_jaeztvgEfr?usp=sharing",'_blank')}}>
-            <img src={Rectangle69} alt="chemistry" />
+          <div className='flex flex-col items-center cursor-pointer' onClick={() => { window.open("https://drive.google.com/drive/folders/1abOl4ymu54b-s4M28tVHv_jaeztvgEfr?usp=sharing", '_blank') }}>
+            <img src={Rectangle69} alt="chemistry" className='w-full h-auto sm:max-w-none max-w-xs' />
           </div>
         </div>
-        <div className='w-1/2 p-[106px]'>
-          <div className='w-[407px] h-[301px] text-5xl font-itim mb-[107px] cursor-pointer' onClick={()=>{window.open("https://drive.google.com/drive/folders/1R2eib53YTVL5-yQfBRewWarM4GgV474w?usp=sharing",'_blank')}}>
-            <img src={Rectangle50} alt="physics" />
+        <div className='flex flex-col items-center p-10 sm:w-1/2 w-full'>
+          <div className='flex flex-col items-center mb-8 cursor-pointer' onClick={() => { window.open("https://drive.google.com/drive/folders/1R2eib53YTVL5-yQfBRewWarM4GgV474w?usp=sharing", '_blank') }}>
+            <img src={Rectangle50} alt="physics" className='w-full h-auto mb-16 sm:max-w-none max-w-xs' />
           </div>
-          <div className='w-[407px] h-[301px] text-5xl font-itim cursor-pointer' onClick={()=>{window.open("https://drive.google.com/drive/folders/1-5E9ZE8uJpXuSCBXtAU1k4zDiNASoKHO?usp=sharing",'_blank')}}>
-            <img src={Rectangle71} alt="biology" />
+          <div className='flex flex-col items-center cursor-pointer' onClick={() => { window.open("https://drive.google.com/drive/folders/1-5E9ZE8uJpXuSCBXtAU1k4zDiNASoKHO?usp=sharing", '_blank') }}>
+            <img src={Rectangle71} alt="biology" className='w-full h-auto sm:max-w-none max-w-xs' />
           </div>
         </div>
       </div>
